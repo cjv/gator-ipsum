71d24aa09e298d9f4de3ba8a7a3bac2271e21142
Courtney Johnston vonNieda <courtney@cremalab.com>
2019-01-14 00:28:56 -0600

    fixing port again

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const routes = require('./router');
 
 // Define the hostname and port where the server can be found
 const hostname = "127.0.0.1";
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 // Define the directory where static files are found
 app.use(express.static('public'));
